aedee1ce81dc5132a006750b39299e569c46684a
arjun <arjunumesh11@gmail.com>
2020-06-17 00:41:55 +0530

    multi_thread

--- main.cpp
+++ main.cpp
@@ -4,30 +4,30 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <iostream>
 #include <fstream>
-#include <sstream>
+#include <errno.h>
 #include "chunkencoding.h"
 #include "http_header.h"
 
 #define PORT 3000
-
-int main(int argc, char const *argv[])
+int check(int status, std::string error) //function to check error
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    int opt = 1, file_size;
-    char buffer_recv[1024] = {0};
-
-    std::string file_name;
+    if (status == -1)
+    {
+        fprintf(stderr, "%s : %i\n", error.c_str(), errno);
+        exit(EXIT_FAILURE);
+    }
+}
+void handle_connection(int new_socket)
+{
+    int file_size;
     std::vector<char> content;
     std::string header;
+    std::string file_name;
     std::vector<char> buffer(1024);
     std::streamsize s;
 
-    file_name = "./test_input/image.jpg";
+    file_name = "./test_input/video.mp4";
 
     std::ifstream file(file_name.c_str(), std::ios::binary);
     if (file.is_open())
@@ -41,48 +41,6 @@ int main(int argc, char const *argv[])
         perror("file not found ");
         exit(0);
     }
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                   &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-             sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = recv(new_socket, buffer_recv, 1024, 0);
-    printf("%s\n", buffer_recv);
 
     header = http_header::make_header(file_name, file_size);
 
@@ -102,5 +60,40 @@ int main(int argc, char const *argv[])
     }
 
     write(new_socket, chunk::END.c_str(), chunk::END.length());
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int opt = 1;
+    char buffer_recv[1024] = {0};
+
+    // Creating socket file descriptor
+    check((server_fd = socket(AF_INET, SOCK_STREAM, 0)), "Soket_failed");
+
+    // Forcefully attaching socket to the port 8080
+    check(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                     &opt, sizeof(opt)),
+          "Set_socket_failed");
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    check(bind(server_fd, (struct sockaddr *)&address,
+               sizeof(address)),
+          "Bind_failed");
+
+    check(listen(server_fd, 3), "Listen_failed");
+    check((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                               (socklen_t *)&addrlen)),
+          "accept_failed");
+
+    valread = recv(new_socket, buffer_recv, 1024, 0);
+    printf("%s\n", buffer_recv);
+    handle_connection(new_socket);
     return 0;
 }
