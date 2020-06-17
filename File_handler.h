236ad59295dc5b890fd74fa92413b279a60bca8b
arjun <arjunumesh11@gmail.com>
2020-06-17 16:46:20 +0530

    file_handling

---
+++ File_handler.h
@@ -0,0 +1,18 @@
+#ifndef _file_handler
+#define _file_handler
+
+#include <ftw.h>
+#include <dirent.h>
+#include <fnmatch.h>
+
+namespace file_handler
+{
+    //create directory  @param std::string root_folder @return -1 if not successfull
+
+    //get file_list     @param : none @returns std::list<string> filelist
+
+    //get file name,path    @param : filename @returns std::string filepath
+
+} // namespace file_handler
+
+#endif
