508d0badd3265243af185b0bd6a70e65b18b306d
arjun <arjunumesh11@gmail.com>
2020-06-17 18:28:24 +0530

    file

--- File_handler.h
+++ File_handler.h
@@ -4,15 +4,31 @@
 #include <ftw.h>
 #include <dirent.h>
 #include <fnmatch.h>
-
+#include <string>
+#include <list>
+#include <unordered_map>
 namespace file_handler
 {
-    //create directory  @param std::string root_folder @return -1 if not successfull
+    class file
+    {
+        //create directory
+        //      @param : std::string root_folder
+        //      @return :-1 if not successfull
+        bool create_directory(std::string root_folder);
 
-    //get file_list     @param : none @returns std::list<string> filelist
+        //get file_list
+        //     @param : none
+        //     @return : std::list<string> filelist
+        std::list<std::string> get_filelist();
 
-    //get file name,path    @param : filename @returns std::string filepath
+        //get file name,path
+        //    @param : std::string filename
+        //    @return : std::string filepath
+        std::string get_file(std::string filename);
 
+    private:
+        std::unordered_map<std::string, std::string> file_list;
+    };
 } // namespace file_handler
 
 #endif
