19e2e0fe6e7d18ae26536809af937c3b3354f7c8
arjun <arjunumesh11@gmail.com>
2020-06-21 00:30:11 +0530

    serve_static

--- include/serve_static.h
+++ include/serve_static.h
@@ -1,10 +1,31 @@
+#ifndef _serve_static
+#define _serve_static
+
 #include <string>
+#include <vector>
+#include <cstring>
+#include "file_handler.h"
 
 namespace serve_static
 {
-    //int static(std::string folder)
-    //@param : folder
-    //@return : -1 if not successfull
-    int serve_static(std::string folder);
+    class servestatic
+    {
+
+        std::vector<std::string> file_paths;
+
+    public:
+        //int static(std::string folder)
+        //@param : folder
+        servestatic(std::string folder);
+
+        //intialise public folder
+        //@param : folder
+        //@return : -1 if not successfull
+        int create_directory(std::string folder);
+
+        int serve(std::string response, int new_socket);
+    };
 
 } // namespace serve_static
+
+#endif
