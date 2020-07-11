565370e21ad9cffb2a8d8f5691a553a5ca928bd8
arjun <arjunumesh11@gmail.com>
2020-07-11 16:08:09 +0530

    header

--- include/http_header.h
+++ include/http_header.h
@@ -8,7 +8,7 @@ namespace http_header
 {
     std::string get_contenttype(std::string filename);
     std::string get_filename(std::string filename);
-    std::string make_header(std::string filename, int file_size);
+    std::string make_header(std::string filename, int file_size, std::string disposition = "inline");
 
 } // namespace http_header
 
